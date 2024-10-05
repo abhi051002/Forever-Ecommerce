f87a86ba56857c149a38ebe8b8ee8e86aa3cb2f2
Abhijit Nanda <abhijit.nanda@hyscaler.com>
2024-10-05 15:36:46 +0530

    hero section added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import Product from "./pages/Product";
+import PlaceOrder from "./pages/PlaceOrder";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/place-order" element={<PlaceOrder />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
